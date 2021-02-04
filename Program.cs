c1eb91aa6ed1bfb7178fb00c6902890da61f6656
DamianEdwards <damian@damianedwards.com>
2021-02-03 16:39:25 -0800

    Update to .NET 5.0

--- Program.cs
+++ Program.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 namespace FuGetGallery
 {
@@ -14,12 +12,13 @@ namespace FuGetGallery
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateHostBuilder (args).Build ().Run ();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder (string[] args) =>
+            Host.CreateDefaultBuilder (args)
+                .ConfigureWebHostDefaults (webBuilder => {
+                    webBuilder.UseStartup<Startup> ();
+                });
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FuGetGallery
 {
@@ -28,7 +29,8 @@ namespace FuGetGallery
         {
             new Database ().MigrateAsync ().Wait ();
             services.AddScoped<Database, Database> ();
-            services.AddMvc().AddRazorPagesOptions(options =>
+            services.AddControllers();
+            services.AddRazorPages(options =>
             {
                 options.Conventions.AddPageRoute("/packages/details", "packages/{id}");
                 options.Conventions.AddPageRoute("/packages/badges", "packages/{id}/badges");
@@ -42,14 +44,14 @@ namespace FuGetGallery
             });
             services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
             services.AddResponseCompression();
-            services.AddHttpClient ("")
+            services.AddHttpClient("")
                 .ConfigurePrimaryHttpMessageHandler (() => new HttpClientHandler {
                     AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
                 });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseResponseCompression();
 
@@ -61,7 +63,14 @@ namespace FuGetGallery
             }
 
             app.UseStaticFiles();
-            app.UseMvc();
+            
+            app.UseRouting();
+            app.UseAuthorization ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+                endpoints.MapRazorPages ();
+            });
         }
     }
 }
